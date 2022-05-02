7a7d2abd4c92adb8c6e67f31c1ea54105305028c
mootqns <mqn360@gmail.com>
2022-05-01 22:46:19 -0700

    many updates

--- final_utils.py
+++ final_utils.py
@@ -44,21 +44,32 @@ def type_stats(df, str):
     ratio =((type_count/len(df))).round(3)
     ratio_df = pd.DataFrame(ratio)
     print(ratio_df)
-
+    print()
     return ratio_df
 
 def pie_chart(df):
-    colors = ("#cd722d", "#fcd053", "#933639","#D3E19F")
+    colors = ("#fca78f", "#ead1dc", "#A7C7E7","#D3E19F")
     wp = { 'linewidth' : 0.75, 'edgecolor' : "black" }
-    explode = (0.05, 0.075, 0.1, 0.25)
+    explode = (0.06, 0.05, 0.075, 0.1)
 
     plt.figure(figsize =(10, 9))
     plt.pie(df["Type"], labels = df.index, colors=colors, wedgeprops=wp, explode=explode)
-    plt.title("Netflix Watch Type Breakdown")
+    plt.title("Netflix Watch Type Ratios")
     plt.tight_layout()
     plt.show()
 
-def bar_chart(df):
+def pie_chart1(df):
+    colors = ("#fca78f","#D3E19F", "#fce9db","#ead1dc", "#bcbcbc", "#A7C7E7")
+    wp = { 'linewidth' : 0.75, 'edgecolor' : "black" }
+    explode = (0.075, 0.0, 0.075, 0.0, 0.075, 0.0)
+
+    plt.figure(figsize =(10, 9))
+    plt.pie(df["Maturity Rating"], labels = df.index, wedgeprops=wp, colors=colors, explode=explode)
+    plt.title("Netflix Maturity Rating Ratios")
+    plt.tight_layout()
+    plt.show()
+
+def barh_chart(df):
     sum_dur_list = df.groupby(['Title'], as_index=False)['Duration'].sum()
 
     plt.figure(figsize =(10, 7))
@@ -69,6 +80,21 @@ def bar_chart(df):
     plt.tight_layout()
     plt.show()
 
+def bar_chart(df):
+    weekday_df = df[~df['Weekday'].isin(['Saturday', 'Sunday'])]
+    weekend_df = df[~df['Weekday'].isin(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])]
+
+    weekday_total_mins = weekday_df['Duration'].sum()
+    weekend_total_mins = weekend_df['Duration'].sum()
+
+    plt.figure(figsize=(10,8))
+    plt.bar(["Weekday", "Weekend"], [weekday_total_mins, weekend_total_mins], color = ["#D3E19F", '#ead1dc'])
+    plt.xlabel("Day Type")
+    plt.ylabel("Minutes Spent Watching")
+    plt.title("Day Type & Total Minutes Spent Watching")
+    plt.tight_layout()
+    plt.show()
+
 def ind_t_test(df1, df2):
     t_computed, p_value = statss.ttest_ind(df1["Duration"], df2["Duration"])
     print("t-computed:", t_computed, "\np-value:", p_value/2)
@@ -76,3 +102,7 @@ def ind_t_test(df1, df2):
         print("Decision:", "fail to reject null")
     if(t_computed > 1.697):
         print("Decision:", "reject null")
+
+def dep_t_test(df1, df2, str):
+    t_computed, p_value = statss.ttest_rel(df1[str], df2[str])
+    print("t-computed:", t_computed, "\np-value:", p_value/2)
